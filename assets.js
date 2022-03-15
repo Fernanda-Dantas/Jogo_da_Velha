f51aa0e32ad47f55b340faf9047620efb05e6a83
Fernanda Dantas <fer.dantas07@gmail.com>
2022-03-15 12:32:11 -0300

    código atualizado

--- assets.js
+++ assets.js
@@ -7,6 +7,63 @@ var nivel = 1;
 var jogadaMaquina = 1;
 var quemComeca = 1;
 
+function maquinaJoga() {
+  if(jogando) {
+    var l,c;
+    if(nivel==1) {
+      do{
+          l = Math.round(Math.random()*2));
+          c = Math.round(Math.random()*2));
+      } while(jogoControle[l][c]!="");
+        jogoControle[l][c]="O";
+    } else if(nivel==2) {
+
+    }
+    verifica = verificaVitoria();
+    if (verifica != "") {
+      alert(verifica + " venceu!");
+      jogando = false;
+    } else if (verifica = "") {
+      alert("Jogo Empatado");
+      jogando = false;
+    }
+    atualizaTabuleiro();
+    quemJoga = 0; 
+    
+  }
+}
+
+function verificaVitoria() {
+  var l,c;
+  // LINHAS
+  for(l = 0; l < 3; l++){
+    if((jogoControle[l][0] == jogoControle[l][1])&&(jogoControle[l][1] == jogoControle[l][2])) {
+      return jogoControle[l][0]; // posição do vencedor
+
+    }
+  } 
+
+  //COLUNAS
+  for(c = 0; c < 3; c++){
+    if(
+      (jogoControle[0][c] == jogoControle[1][c]) && (jogoControle[1][c] == jogoControle[2][c])) {
+        return jogoControle[0][c]; // posição do vencedor
+    }
+  }
+
+  //DIAGONAIS
+  if(
+    (jogoControle[0][0] == jogoControle[1][1]) && (jogoControle[1][1] == jogoControle[2][2])) {
+      return jogoControle[0][0]; // posição do vencedor
+  }
+  if(
+    (jogoControle[0][2] == jogoControle[1][1]) && (jogoControle[1][1] == jogoControle[2][0])) {
+      return jogoControle[0][2]; // posição do vencedor
+  }
+  return ""; // indica que não houve vencedor
+}
+
+
 function jogar(posicao) {
   if ((jogando) && (quemJoga == 0)) {
       switch(posicao) {
@@ -65,8 +122,21 @@ function jogar(posicao) {
           }
         break;     
       }
-      atualizaTabuleiro();
-  }
+
+    if (quemJoga == 1) {
+        atualizaTabuleiro();
+        verifica = verificaVitoria();
+        if (verifica != "") {
+          alert(verifica + " venceu!");
+          jogando = false;
+        } else if (verifica = "") {
+          alert("Jogo Empatado");
+          jogando = false;
+        }
+        maquinaJoga();
+    }  
+  }  
+  
 }
 
 
